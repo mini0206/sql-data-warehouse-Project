c5b6e9941e2ed82ce6a8a75d4bed521328429474
mini0206 <61971459+mini0206@users.noreply.github.com>
2026-08-26 19:50:15 +0700

    Update silver_ddl.sql

--- scripts/silver/silver_ddl.sql
+++ scripts/silver/silver_ddl.sql
@@ -76,8 +76,8 @@ CREATE TABLE silver.crm_prd_info (
     prd_nm       NVARCHAR(50),
     prd_cost     INT,
     prd_line     NVARCHAR(50),
-    prd_start_dt DATETIME,
-    prd_end_dt DATETIME,
+    prd_start_dt DATE,
+    prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
@@ -98,16 +98,15 @@ CREATE TABLE silver.crm_sales_details (
     sls_ord_num  NVARCHAR(50),
     sls_prd_key  NVARCHAR(50),
     sls_cust_id  INT,
-    sls_order_dt INT,
-    sls_ship_dt  INT,
-    sls_due_dt   INT,
+    sls_order_dt DATE,
+    sls_ship_dt  DATE,
+    sls_due_dt   DATE,
     sls_sales    INT,
     sls_quantity INT,
     sls_price    INT,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 );
 GO
-
 
 -- ============================================================================
 -- ERP: Location Information
